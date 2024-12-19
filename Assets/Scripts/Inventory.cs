fdfabdeb69ff7db7ac75a325fa267e370e34ba01
Kadir Tiryaki <abdulkadirtiryaki2090@gmail.com>
2024-12-20 02:04:42 +0300

    Inventory UI

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -5,7 +5,31 @@ using UnityEngine;
 public class Inventory : MonoBehaviour
 {
     public SCInventory playerInventory;
-
+    InventoryUIController inventoryUIController;
+    bool isSwapping;
+    int tempIndex;
+    Slot tempSlot;
+    private void Start()
+    {
+        inventoryUIController = gameObject.GetComponent<InventoryUIController>();
+        inventoryUIController.UpdateUI();
+    }
+    public void SwapItem(int index)
+    {
+        if(isSwapping == false)
+        {
+            tempIndex = index;
+            tempSlot = playerInventory.inventorySlots[tempIndex];
+            isSwapping = true;
+        }
+        else if(isSwapping == true)
+        {
+            playerInventory.inventorySlots[tempIndex] = playerInventory.inventorySlots[index];
+            playerInventory.inventorySlots[index] = tempSlot;
+            isSwapping=false;
+        }
+        inventoryUIController.UpdateUI();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Item"))
@@ -13,6 +37,7 @@ public class Inventory : MonoBehaviour
             if (playerInventory.AddItem(other.gameObject.GetComponent<Item>().item))
             {
                 Destroy(other.gameObject);
+                inventoryUIController.UpdateUI();
             }
             
         }
